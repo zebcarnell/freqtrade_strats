dd627c97aa3a1c5dfe86d22d71998730912daa08
Zeb <zebcarnell@gmail.com>
2024-09-06 00:01:00 +1200

    updates

--- stratscorer/private.py
+++
@@ -1,8 +0,0 @@
-# FTP_USERNAME = ' '
-# FTP_PASSWORD = ' '
-
-league_dir = "./logs/strategy_league/"
-filename = "./logs/strategy_league/strategy_league.csv"
-ftp_server = "web0151.zxcs.nl"
-# remote_path = "/domains/dutchalgotrading.com/public_html/wp-content/uploads/stratscorer/"
-remote_path = "/"
